0ed5b4d82df58ab6c1561be6939e8877dc2ee493
rmstibbard <rmstibbard@users.noreply.github.com>
2016-01-21 22:32:45 +0000

    Create mysql01.sql

---
+++ mysql01/mysql01.sql
@@ -0,0 +1,31 @@
+// Display list of databases on the system:
+SHOW DATABASES;
+
+// Display MySQL help topics:
+help
+
+// or:
+?
+
+// or: 
+\?
+
+// Print command before it is finished:
+SHOW DATABASES
+\p
+;
+
+// Clear a commad before it is finished:
+SHOW DATBASES
+\c
+
+// Quit the MySQL prompt:
+exit
+
+// or
+quit
+
+// or
+\q
+
+
